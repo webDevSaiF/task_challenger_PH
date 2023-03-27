7545ac7aee902298cefbf4bc520efa47eb8d50a6
iSaiF99 <isaif041@gmail.com>
2023-03-27 13:47:23 +0600

    First Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// RECEIVING DATA
+async function loadData() {
+  const response = await fetch("http://www.boredapi.com/api/activity/");
+  const data = await response.json();
+  return data;
+}
+loadData()
+  .then((data) => printData(data))
+  .catch((err) => console.log("Could Not Fetch the data from API"));
+
+// PRINTING DATA
+function printData(obj) {
+  const activityText = document.querySelector(".activity-box");
+  activityText.textContent = obj.activity;
+  console.log(obj);
+}
