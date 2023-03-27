b234e69118a4beab674d6db1e632bc72a575394c
iSaiF99 <isaif041@gmail.com>
2023-03-27 13:58:42 +0600

    error solved

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // RECEIVING DATA
 async function loadData() {
-  const response = await fetch("http://www.boredapi.com/api/activity/");
+  const response = await fetch("//www.boredapi.com/api/activity/");
   const data = await response.json();
   return data;
 }
